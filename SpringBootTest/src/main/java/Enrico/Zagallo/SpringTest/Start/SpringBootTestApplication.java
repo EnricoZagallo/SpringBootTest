f49699f64c379acf680cfa3955ea17de37b339c3
Enrico Zagallo <enricozagallo92@gmail.com>
2026-03-20 09:44:52 -0300

    Spring boot minimum to boot

---
+++ SpringBootTest/src/main/java/Enrico/Zagallo/SpringTest/Start/SpringBootTestApplication.java
@@ -0,0 +1,15 @@
+package Enrico.Zagallo.SpringTest.Start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "Enrico.Zagallo.SpringTest")
+public class SpringBootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestApplication.class, args);
+	}
+
+}
